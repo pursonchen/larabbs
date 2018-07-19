ca9704084d0a09fa8fee253e0f3458fcfce9b757
vagrant <vagrant@homestead>
2018-07-19 12:09:22 +0000

    users show

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -114,4 +114,8 @@ class UsersController extends Controller
             ])
             ->setStatusCode(201);
     }
+
+    public function show(User $user) {
+        return $this->response->item($user, new UserTransformer());
+    }
 }
